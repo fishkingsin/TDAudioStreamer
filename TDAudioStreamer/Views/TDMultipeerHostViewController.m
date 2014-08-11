4c3485dffc7a6a4ce33df78440169cb24a5aa12e
Kong king sin <fishkingsin@gmail.com>
2014-08-11 10:11:22 +0800

    blocked invalid iCloud item

--- TDAudioStreamer/Views/TDMultipeerHostViewController.m
+++ TDAudioStreamer/Views/TDMultipeerHostViewController.m
@@ -69,15 +69,15 @@
     [self.session sendData:[NSKeyedArchiver archivedDataWithRootObject:[info copy]]];
 
     NSArray *peers = [self.session connectedPeers];
-    NSURL* assetURL = [self.song valueForProperty:MPMediaItemPropertyAssetURL];
+
     NSString *title = [self.song valueForProperty:MPMediaItemPropertyTitle];
-    if (!assetURL) {
+    if ([[self.song valueForProperty:MPMediaItemPropertyIsCloudItem] boolValue]) {
         /*
          * !!!: When MPMediaItemPropertyAssetURL is nil, it typically means the file
          * in question is protected by DRM. (old m4p files)
          */
         NSString *msg = [NSString stringWithFormat:@"%@ has DRM track will not be played",title];
-        NSLog(@"%@",msg);
+        DDLogVerbose(@"%@",msg);
         UIAlertView *alert = [[UIAlertView alloc]initWithTitle:@"Error" message:msg delegate:nil cancelButtonTitle:@"Cancel" otherButtonTitles:@"OK", nil];
         [alert show];
         return;
